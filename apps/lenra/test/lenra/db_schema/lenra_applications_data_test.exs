512c7e569d0e12333c183d3a1b12fefc741482a1
Emric Pichonnier <emric.pichonnier@lenra.me>
2021-10-21 09:38:44 +0200

    Add test

--- apps/lenra/test/lenra/db_schema/lenra_applications_data_test.exs
+++ apps/lenra/test/lenra/db_schema/lenra_applications_data_test.exs
@@ -1,7 +1,7 @@
 defmodule Lenra.LenraApplicationDataTest do
   use Lenra.RepoCase, async: true
 
-  alias Lenra.{ApplicationsData, ApplicationsData, ApplicationsUsersData, LenraApplicationServices}
+  alias Lenra.{ApplicationsData, ApplicationsData, LenraApplicationServices}
 
   setup do
     {:ok, data: create_application()}
@@ -30,12 +30,19 @@ defmodule Lenra.LenraApplicationDataTest do
     test "new/2 create applications data for user", %{
       data: %{application: application, user_uuid: _user_id}
     } do
-      {:ok, appli} = Repo.insert(ApplicationsData.new(application, %{json_data: %{test: "test"}}))
+      {:ok, inserted_app_data} = Repo.insert(ApplicationsData.new(application, %{json_data: %{test: "test"}}))
 
-      {:ok, app} = Repo.fetch_by(ApplicationsData, %{id: appli.id})
+      {:ok, app_data} = Repo.fetch_by(ApplicationsData, %{id: inserted_app_data.id})
 
       application_id = application.id
-      assert %{application_id: application_id, json_data: %{"test" => "test"}} = app
+      assert %{application_id: application_id, json_data: %{"test" => "test"}} = app_data
+    end
+
+    test "new/2 with invalid data should failed", %{
+      data: %{application: application, user_uuid: _user_id}
+    } do
+      app_data = Repo.insert(ApplicationsData.new(application, %{data: %{test: "test"}}))
+      assert {:error, %{errors: [json_data: _error_message]}} = app_data
     end
   end
 end
