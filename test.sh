11cd8a7865d288477d8abf9931fa57659c00732b
ClementGld <clement.guillaud@lenra.io>
2022-02-02 11:45:01 +0100

    ajout fichier de test

---
+++ test.sh
@@ -0,0 +1,23 @@
+export PORT=4000 
+export SECRET_KEY_BASE=FuEn07fjnCLaC53BiDoBagPYdsv/S65QTfxWgusKP1BA5NiaFzXGYMHLZ6JAYxt1
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_DB=lenra_dev
+export POSTGRES_HOST=localhost
+export FAAS_URL=https://openfaas-dev.lenra.me
+export FAAS_AUTH=Basic YWRtaW46Z0Q4VjNHR1YxeUpS
+export FAAS_REGISTRY=registry.gitlab.com/lenra/platform/lenra-ci
+export RUNNER_SECRET=sZWshq6h0RNO9T1GgUnzLmPpDkSkDAoukmd30mTuwQAGIHYIIVdl7VD2h305
+export ENVIRONMENT=prod
+export APP_HOST=localhost
+export GITLAB_API_URL=https://gitlab.com/api/v4
+export GITLAB_API_TOKEN=Zuz-dZc834q3CtU-bnX5
+export GITLAB_PROJECT_ID=26231009
+export SERVICE_NAME=1324
+export SENDGRID_API_KEY=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
+export GUARDIAN_SECRET_KEY=5oIBVh2Hauo3LT4knNFu29lX9DYu74SWZfjZzYn+gfr0aryxuYIdpjm8xd0qGGqK
+export SENTRY_DSN=648664
+export ENVIRONMENT=46546
+
+
+_build/prod/rel/lenra/bin/lenra start
