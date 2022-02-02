9aa4fad8f8de757d528bbc8bab23cf221ce8f015
ClementGld <clement.guillaud@lenra.io>
2022-02-02 11:24:40 +0100

    add email_service for register and recovery email

--- apps/lenra/lib/lenra/services/email_services.ex
+++ apps/lenra/lib/lenra/services/email_services.ex
@@ -1,22 +1,30 @@
-# defmodule Lenra.EmailService do
-#   @moduledoc false
+defmodule Lenra.EmailService do
+  @moduledoc false
 
-#   import Bamboo.Email
+  import Bamboo.Email
+  alias Lenra.Mailer
+  alias Bamboo.SendGridHelper
 
-#   def welcome_text_email(email_address, code) do
-#     # base template ID : d-bd160809d9a04b07ac6925a823f8f61c
-#     new_email()
-#     |> to(email_address)
-#     |> from("subscription@lenra.me")
-#     |> subject("Bienvenue!")
-#     |> text_body("Bienvenue chez Lenra " <> email_address <> " ! " <> "Votre code: " <> code)
-#   end
+  def welcome_text_email(email_address, code) do
+    # base template ID : d-bd160809d9a04b07ac6925a823f8f61c
+    new_email()
+    |> to(email_address)
+    |> from("subscription@lenra.me")
+    |> subject("Bienvenue!")
+    |> SendGridHelper.with_template("d-bd160809d9a04b07ac6925a823f8f61c")
+    |> SendGridHelper.add_dynamic_field("title", "Bienvenue Chez Lenra " <> email_address <> "!")
+    |> SendGridHelper.add_dynamic_field("message", "Votre code : " <> code)
+    |> Mailer.deliver_now()
+  end
 
-#   def recovery_email(email_address, code) do
-#     new_email()
-#     |> to(email_address)
-#     |> from("verification@lenra.me")
-#     |> subject("Votre code de vérification!")
-#     |> text_body("Bonjour " <> email_address <> " ! " <> "Votre code: " <> code)
-#   end
-# end
+  def recovery_email(email_address, code) do
+    new_email()
+    |> to(email_address)
+    |> from("verification@lenra.me")
+    |> subject("Votre code de vérification!")
+    |> SendGridHelper.with_template("d-bd160809d9a04b07ac6925a823f8f61c")
+    |> SendGridHelper.add_dynamic_field("title", "Bonjour " <> email_address)
+    |> SendGridHelper.add_dynamic_field("message", "Votre code : " <> code)
+    |> Mailer.deliver_now()
+  end
+end
--- apps/lenra/lib/lenra/services/user_services.ex
+++ apps/lenra/lib/lenra/services/user_services.ex
@@ -4,7 +4,7 @@ defmodule Lenra.UserServices do
   """
   import Ecto.Query, only: [from: 2]
 
-  alias Lenra.{Repo, User, Password, DevCode, RegistrationCodeServices}
+  alias Lenra.{Repo, User, Password, DevCode, RegistrationCodeServices, EmailWorker}
 
   @doc """
     Register a new user, save him to the database. The email must be unique. The password is hashed before inserted to the database.
@@ -26,7 +26,7 @@ defmodule Lenra.UserServices do
       end
     )
     # send verification email disabled
-    # |> Ecto.Multi.run(:add_event, &add_registration_events/2)
+    |> Ecto.Multi.run(:add_event, &add_registration_events/2)
     |> Repo.transaction()
   end
 
@@ -35,12 +35,12 @@ defmodule Lenra.UserServices do
   end
 
   # send verification email disabled
-  # defp add_registration_events(_repo, %{
-  #       inserted_registration_code: registration_code,
-  #       inserted_user: user
-  #     }) do
-  #  EmailWorker.add_email_verification_event(user, registration_code.code)
-  # end
+  defp add_registration_events(_repo, %{
+         inserted_registration_code: registration_code,
+         inserted_user: user
+       }) do
+    EmailWorker.add_email_verification_event(user, registration_code.code)
+  end
 
   def update(user, params) do
     Ecto.Multi.new()
