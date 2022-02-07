4c8bcb11d9a46d3aa0ca20d4a035e9be180f98d9
Emric Pichonnier <emric.pichonnier@lenra.me>
2022-02-07 13:28:20 +0100

    fix: Openfaas deploy build anyway (#46)

--- apps/lenra_web/lib/lenra_web/controllers/runner_controller.ex
+++ apps/lenra_web/lib/lenra_web/controllers/runner_controller.ex
@@ -4,12 +4,20 @@ defmodule LenraWeb.RunnerController do
 
   alias Lenra.{BuildServices, DeploymentServices}
 
-  def update_build(conn, %{"id" => build_id, "status" => status}) do
+  defp maybe_deploy_in_main_env(build, "success"), do: DeploymentServices.deploy_in_main_env(build)
+
+  defp maybe_deploy_in_main_env(_build, "failure"), do: {:ok, :not_deployed}
+
+  def update_build(conn, %{"id" => build_id, "status" => status}) when status in ["success", "failure"] do
     with {:ok, build} <- BuildServices.fetch(build_id),
          {:ok, _} <- BuildServices.update(build, %{status: status}),
-         {:ok, _} <- DeploymentServices.deploy_in_main_env(build) do
+         {:ok, _} <- maybe_deploy_in_main_env(build, status) do
       conn
       |> reply
     end
+  end
+
+  def update_build(_conn, _) do
+    {:error, :invalid_build_status}
   end
 end
