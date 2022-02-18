f677d86a715699b59287ffaf424e65fe55b92a05
jonas-martinez <cbobbxta.fr@gmail.com>
2022-02-18 17:48:18 +0100

    Fix formatting

--- apps/lenra/priv/repo/migrations/20220218161250_add_is_public_to_env.exs
+++ apps/lenra/priv/repo/migrations/20220218161250_add_is_public_to_env.exs
@@ -3,7 +3,7 @@ defmodule Lenra.Repo.Migrations.AddIsPublicToEnv do
 
   def change do
     alter table(:environments) do
-        add(:is_public, :boolean, default: false, null: false)
+      add(:is_public, :boolean, default: false, null: false)
     end
   end
 end
