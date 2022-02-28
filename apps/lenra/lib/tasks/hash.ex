daefaed9d56179cbb8b029c60208991184ebc461
ClementGld <clement.guillaud@lenra.io>
2022-02-28 15:49:31 +0100

    modify error

--- apps/lenra/lib/tasks/hash.ex
+++ apps/lenra/lib/tasks/hash.ex
@@ -1,15 +1,28 @@
 defmodule Mix.Tasks.Hash do
-  @moduledoc "The hash mix task: `mix help hash`"
+  @moduledoc "The mix hash task
+
+  to use : mix hash file_name
+
+  properties : --algo {desired algo to hash the file}"
   use Mix.Task
 
-  @shortdoc "Task for hash a file in sha256"
+  @shortdoc "Task use to hash a file"
   def run(args) do
     {opts, paths} = OptionParser.parse!(args, strict: [algo: :string])
-    algo = Keyword.get(opts, :algo, "md5")
-      File.stream!(paths, [], 2048)
-      |> Enum.reduce(:crypto.hash_init(String.to_atom(algo)), &:crypto.hash_update(&2, &1))
-      |> :crypto.hash_final()
-      |> Base.encode16()
-      |> IO.puts()
+    case paths do
+      [] ->
+        IO.puts("no argument found. use mix help hash for more information")
+      [paths] ->
+        algo = Keyword.get(opts, :algo, "md5")
+        File.stream!(paths, [], 2048)
+        |> Enum.reduce(:crypto.hash_init(String.to_atom(algo)), &:crypto.hash_update(&2, &1))
+        |> :crypto.hash_final()
+        |> Base.encode16()
+        |> IO.puts()
+      _ ->
+        IO.puts("too much arguments. use mix help hash for more information")
+
+    end
+
   end
 end
--- apps/lenra/test/tasks/lenra_tasks_test.exs
+++ apps/lenra/test/tasks/lenra_tasks_test.exs
@@ -13,5 +13,19 @@ defmodule Mix.Tasks.HashTest do
       assert hash == "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
       File.rm(@path)
     end
+
+    test "hash/1 test mix hash with no argument" do
+
+      hash = capture_io(fn -> Hash.run([]) end) |> String.trim() |> String.downcase()
+
+      assert hash == "no argument found. use mix help hash for more information"
+    end
+
+    test "hash/1 test mix hash with too much arguments" do
+
+      hash = capture_io(fn -> Hash.run(["test", "test"]) end) |> String.trim() |> String.downcase()
+
+      assert hash == "too much arguments. use mix help hash for more information"
+    end
   end
 end
