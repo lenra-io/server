12f8cafe582b25e7aaecd107df3c69f7cc8268b0
ClementGld <clement.guillaud@lenra.io>
2022-02-28 17:14:18 +0100

    feat: create mix task to transform markdownin html

---
+++ apps/lenra/lib/tasks/markdown_to_html.ex
@@ -0,0 +1,20 @@
+defmodule Mix.Tasks.Transform do
+  @moduledoc "The mix transform task
+  to use : mix transform file_name"
+  use Mix.Task
+
+  @shortdoc "Task use to transform a markdown file in html file"
+  def run(args) do
+    case args do
+      [] ->
+        IO.puts("no argument found. use mix help transform for more information")
+      [args] ->
+        html = File.read!(args)
+        |> Earmark.as_html!()
+        path = Path.rootname(args) <> ".html"
+        File.write!(path, html)
+      _ ->
+        IO.puts("too much arguments. use mix help transform for more information")
+    end
+  end
+end
---
+++ apps/lenra/test/tasks/lenra_task_transform_test.exs
@@ -0,0 +1,32 @@
+defmodule Mix.Tasks.TransformTest do
+  use ExUnit.Case, async: true
+  import ExUnit.CaptureIO
+  alias Mix.Tasks.Transform
+
+  @path1 "/tmp/transform_test.md"
+  @path2 "/tmp/transform_test.html"
+  describe "mix tasks" do
+    test "transform/1 test transform markdown file to html file" do
+
+      File.write!(@path1, "test")
+      Transform.run([@path1])
+      assert File.exists?(@path2)
+      File.rm!(@path1)
+      File.rm(@path2)
+    end
+
+    test "transform/1 test mix transform with no argument" do
+
+      hash = capture_io(fn -> Transform.run([]) end) |> String.trim() |> String.downcase()
+
+      assert hash == "no argument found. use mix help transform for more information"
+    end
+
+    test "transform/1 test mix transform with too much arguments" do
+
+      hash = capture_io(fn -> Transform.run(["test", "test"]) end) |> String.trim() |> String.downcase()
+
+      assert hash == "too much arguments. use mix help transform for more information"
+    end
+  end
+end
