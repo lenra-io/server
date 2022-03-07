4a38d85ee0c682400730e59d126fc223b48364ec
Clément Guillaud <91874264+ClementGld@users.noreply.github.com>
2022-03-07 11:49:03 +0100

    feat: create a mix task for hashage (#99)

---
+++ apps/lenra/lib/tasks/hash.ex
@@ -0,0 +1,38 @@
+defmodule Mix.Tasks.Hash do
+  @shortdoc "Task use to hash a file"
+
+  @moduledoc "The mix hash task
+
+  to use : mix hash file_name
+
+  properties : --algo {desired algorithm to hash the file}"
+  use Mix.Task
+
+  def run(args) do
+    {opts, paths} = OptionParser.parse!(args, strict: [algo: :string])
+
+    case paths do
+      [] ->
+        IO.puts("no argument found. use mix help hash for more information")
+
+      [paths] ->
+        case File.exists?(paths) do
+          true ->
+            algo = Keyword.get(opts, :algo, "md5")
+
+            paths
+            |> File.stream!([], 2048)
+            |> Enum.reduce(:crypto.hash_init(String.to_existing_atom(algo)), &:crypto.hash_update(&2, &1))
+            |> :crypto.hash_final()
+            |> Base.encode16()
+            |> IO.puts()
+
+          false ->
+            IO.puts("The file does not exist or the path is invalid")
+        end
+
+      _foo ->
+        IO.puts("too much arguments. use mix help hash for more information")
+    end
+  end
+end
---
+++ apps/lenra/test/tasks/lenra_tasks_test.exs
@@ -0,0 +1,53 @@
+defmodule Mix.Tasks.HashTest do
+  use ExUnit.Case, async: true
+  import ExUnit.CaptureIO
+  alias Mix.Tasks.Hash
+
+  @path "/tmp/hash_test"
+  describe "mix tasks" do
+    test "hash/1 test generating hash with mix hash" do
+      File.write!(@path, "test")
+      hash = capture_io(fn -> Hash.run([@path, "--algo", "sha256"]) end)
+
+      hash1 =
+        hash
+        |> String.trim()
+        |> String.downcase()
+
+      assert hash1 == "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
+      File.rm(@path)
+    end
+
+    test "hash/1 test mix hash with no argument" do
+      hash = capture_io(fn -> Hash.run([]) end)
+
+      hash1 =
+        hash
+        |> String.trim()
+        |> String.downcase()
+
+      assert hash1 == "no argument found. use mix help hash for more information"
+    end
+
+    test "hash/1 test mix hash with too much arguments" do
+      hash = capture_io(fn -> Hash.run(["test", "test"]) end)
+
+      hash1 =
+        hash
+        |> String.trim()
+        |> String.downcase()
+
+      assert hash1 == "too much arguments. use mix help hash for more information"
+    end
+
+    test "hash/1 test mix hash with invlaid path" do
+      hash = capture_io(fn -> Hash.run([@path]) end)
+
+      hash1 =
+        hash
+        |> String.trim()
+
+      assert hash1 == "The file does not exist or the path is invalid"
+    end
+  end
+end
