b6d0a31d7a501cc1063b54aa043693994808749b
ClementGld <clement.guillaud@lenra.io>
2022-03-08 13:44:37 +0100

    begining

--- apps/lenra/test/lenra/db_schema/user_accept_cgu_version_test.exs
+++ apps/lenra/test/lenra/db_schema/user_accept_cgu_version_test.exs
@@ -4,7 +4,7 @@ defmodule Lenra.UserAcceptCguVersionTest do
   alias Lenra.{Cgu, UserAcceptCguVersion}
 
   @valid_cgu %{link: "Test", version: "1.0.0", hash: "test"}
-
+  @valid_cgu1 %{link: "test", version: "2.0.0", hash: "Test"}
   describe "lenra_user_accept_cgu_version" do
     test "new/1 with valid data creates a user_accept_cgu_version" do
       {:ok, %{inserted_user: user}} = UserTestHelper.register_john_doe()
@@ -21,6 +21,43 @@ defmodule Lenra.UserAcceptCguVersionTest do
     test "new/1 with invalid data creates an invalid user_accept_cgu_version" do
       assert %{changes: _user_accept_cgu_version1, valid?: false} =
                UserAcceptCguVersion.new(%{user_id: nil, cgu_id: nil})
+    end
+
+    test "new/1 can't add 2 same cgu_id and user_id in the database" do
+      {:ok, %{inserted_user: user}} = UserTestHelper.register_john_doe()
+      {:ok, %Cgu{} = inserted_cgu} = @valid_cgu |> Cgu.new() |> Repo.insert()
+
+      assert %{changes: user_accept_cgu_version, valid?: true} =
+               UserAcceptCguVersion.new(%{user_id: user.id, cgu_id: inserted_cgu.id})
+
+      IO.inspect(user_accept_cgu_version)
+
+      assert %{changes: _user_accept_cgu_version, valid?: false} =
+               UserAcceptCguVersion.new(%{user_id: user.id, cgu_id: inserted_cgu.id})
+    end
+
+    test "new/1 can add 2 same cgu_id for 2 different user_id in the database" do
+      {:ok, %{inserted_user: user}} = UserTestHelper.register_john_doe()
+      {:ok, %Cgu{} = inserted_cgu} = @valid_cgu |> Cgu.new() |> Repo.insert()
+      {:ok, %Cgu{} = inserted_cgu1} = @valid_cgu1 |> Cgu.new() |> Repo.insert()
+
+      assert %{changes: _user_accept_cgu_version, valid?: true} =
+               UserAcceptCguVersion.new(%{user_id: user.id, cgu_id: inserted_cgu.id})
+
+      assert %{changes: _user_accept_cgu_version, valid?: true} =
+               UserAcceptCguVersion.new(%{user_id: user.id, cgu_id: inserted_cgu1.id})
+    end
+
+    test "new/1 can add 2 same user_id for 2 different cgu_id in the database" do
+      {:ok, %{inserted_user: user}} = UserTestHelper.register_john_doe()
+      {:ok, %Cgu{} = inserted_cgu} = @valid_cgu |> Cgu.new() |> Repo.insert()
+      {:ok, %Cgu{} = inserted_cgu1} = @valid_cgu1 |> Cgu.new() |> Repo.insert()
+
+      assert %{changes: _user_accept_cgu_version, valid?: true} =
+               UserAcceptCguVersion.new(%{user_id: user.id, cgu_id: inserted_cgu.id})
+
+      assert %{changes: _user_accept_cgu_version, valid?: true} =
+               UserAcceptCguVersion.new(%{user_id: user.id, cgu_id: inserted_cgu1.id})
     end
   end
 end
