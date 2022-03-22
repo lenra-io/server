a4313adf0a47d7a8622f72e141f76261ec30d8f3
Emric Pichonnier <emric.pichonnier@lenra.me>
2022-03-22 14:55:37 +0100

    trigger & down

--- apps/lenra/priv/repo/migrations/20220318083741_new_data_schema.exs
+++ apps/lenra/priv/repo/migrations/20220318083741_new_data_schema.exs
@@ -1,7 +1,7 @@
 defmodule Lenra.Repo.Migrations.NewDataSchema do
   use Ecto.Migration
 
-  def change do
+  def up() do
     create table(:temp_datastores) do
       add(:user_id, :int)
       add(:application_id, :id)
@@ -12,8 +12,7 @@ defmodule Lenra.Repo.Migrations.NewDataSchema do
     end
 
     execute(
-      "INSERT INTO temp_datastores(user_id, application_id, data, inserted_at, updated_at) SELECT user_id, application_id, data, inserted_at, updated_at FROM datastores",
-      "INSERT INTO datastores(user_id, application_id, data, inserted_at, updated_at) SELECT user_id, application_id, data, inserted_at, updated_at FROM temp_datastores"
+      "INSERT INTO temp_datastores(user_id, application_id, data, inserted_at, updated_at) SELECT user_id, application_id, data, inserted_at, updated_at FROM datastores"
     )
 
     drop(table(:datastores))
@@ -52,8 +51,24 @@ defmodule Lenra.Repo.Migrations.NewDataSchema do
 
     create(unique_index(:data_references, [:refs_id, :refBy_id], name: :data_references_refs_id_refBy_id))
 
+    execute("CREATE FUNCTION check_user_datas() RETURNS trigger LANGUAGE plpgsql as $$
+        DECLARE
+        env_id bigint;
+        BEGIN
+          SELECT ds.environment_id INTO env_id FROM datastores AS ds LEFT JOIN datas AS d ON d.datastore_id = ds.id WHERE NEW.data_id = d.id;
+          IF 1 <= (SELECT COUNT(*) FROM datas AS d LEFT JOIN user_datas AS u ON u.data_id = d.id LEFT JOIN datastores AS ds ON ds.id = d.datastore_id WHERE new.user_id = u.user_id AND ds.environment_id = env_id GROUP BY ds.environment_id) THEN
+            RAISE EXCEPTION 'user already have user_data for this environment';
+          END IF;
+          RETURN NEW;
+        END;
+    $$")
+
+    execute("CREATE TRIGGER only_one_user_data_per_environment
+    BEFORE INSERT OR UPDATE ON user_datas
+    FOR EACH ROW EXECUTE procedure check_user_datas()")
+
     execute(
-      "INSERT INTO datastores(environment_id,name, inserted_at, updated_at) SELECT env.id, 'UserDatas', t.inserted_at, t.updated_at FROM environments AS env LEFT JOIN temp_datastores AS t ON t.application_id = env.application_id"
+      "INSERT INTO datastores(environment_id, name, inserted_at, updated_at) SELECT env.id, 'UserDatas', t.inserted_at, t.updated_at FROM environments AS env LEFT JOIN temp_datastores AS t ON t.application_id = env.application_id"
     )
 
     execute(
@@ -65,5 +80,76 @@ defmodule Lenra.Repo.Migrations.NewDataSchema do
     )
 
     drop(table(:temp_datastores))
+  end
+
+  def down() do
+    create table(:temp_datastores) do
+      add(:environment_id, :id, null: false)
+      add(:name, :string)
+
+      timestamps()
+    end
+
+    create table(:temp_datas) do
+      add(:datastore_id, :id, null: false)
+      add(:data, :map, null: false)
+
+      timestamps()
+    end
+
+    create table(:temp_user_datas) do
+      add(:user_id, :id, null: false)
+      add(:data_id, :id, null: false)
+
+      timestamps()
+    end
+
+    execute(
+      "INSERT INTO temp_datastores(environment_id, name, inserted_at, updated_at) SELECT environment_id, name, inserted_at, updated_at FROM datastores"
+    )
+
+    execute(
+      "INSERT INTO temp_datas(datastore_id, data, inserted_at, updated_at) SELECT datastore_id, data, inserted_at, updated_at FROM datas"
+    )
+
+    execute(
+      "INSERT INTO temp_user_datas(user_id, data_id, inserted_at, updated_at) SELECT user_id, data_id, inserted_at, updated_at FROM user_datas"
+    )
+
+    execute("DROP TRIGGER only_one_user_data_per_environment ON user_datas")
+
+    execute("DROP FUNCTION check_user_datas")
+
+    drop(unique_index(:data_references, [:refs_id, :refBy_id], name: :data_references_refs_id_refBy_id))
+
+    drop(table(:data_references))
+
+    drop(unique_index(:user_datas, [:user_id, :data_id], name: :user_datas_user_id_data_id))
+
+    drop(table(:user_datas))
+
+    drop(table(:datas))
+
+    drop(table(:datastores))
+
+    create table(:datastores) do
+      add(:user_id, references(:users))
+      add(:application_id, references(:applications))
+      add(:data, :map)
+      timestamps()
+    end
+
+    create(unique_index(:datastores, [:user_id, :application_id], name: :datastores_user_id_application_id_index))
+
+    execute(
+      "INSERT INTO datastores(user_id, application_id, data, inserted_at, updated_at) SELECT u.user_id, main.application_id, d.data, t.inserted_at, t.updated_at FROM temp_datastores AS t
+      LEFT JOIN application_main_environment AS main ON main.environment_id = t.environment_id
+      LEFT JOIN temp_datas AS d ON d.datastore_id = t.id
+      LEFT JOIN temp_user_datas AS u ON d.id = u.data_id"
+    )
+
+    drop(table(:temp_datastores))
+    drop(table(:temp_datas))
+    drop(table(:temp_user_datas))
   end
 end
