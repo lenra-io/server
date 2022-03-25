8fdfdb94f04d1ce8b5330af6100bd2786f0efab8
Emric Pichonnier <emric.pichonnier@gmail.com>
2022-03-25 12:29:58 +0100

    feat: remove get_old_data & upsert_data (#173)

--- apps/lenra/lib/lenra/services/datastore_services.ex
+++ apps/lenra/lib/lenra/services/datastore_services.ex
@@ -14,29 +14,4 @@ defmodule Lenra.DatastoreServices do
   def get_by(clauses) do
     Repo.get_by(Datastore, clauses)
   end
-
-  @doc """
-    Gets the datastore data
-
-    Returns the data.
-    data is nil if the data does not exists for this user/app
-  """
-  def get_old_data(user_id, application_id) do
-    get_by(user_id: user_id, application_id: application_id)
-  end
-
-  @doc """
-  Creates or updates the data in the corresponding datastore.
-
-  Returns `{:ok, struct}` if the data was successfully inserted or updated.
-  Returns `{:error, changeset}` if the data could not be inserted or updated.
-  """
-  def upsert_data(user_id, application_id, data) do
-    user_id
-    |> Datastore.new(application_id, data)
-    |> Repo.insert(
-      on_conflict: [set: [data: data]],
-      conflict_target: [:user_id, :application_id]
-    )
-  end
 end
