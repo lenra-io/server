344d1a252b43534c188ae8548a7d0d996441a8b4
Emric Pichonnier <emric.pichonnier@gmail.com>
2022-03-25 15:43:15 +0100

    feat: create datastore service (#174)

--- apps/lenra/lib/lenra/services/datastore_services.ex
+++ apps/lenra/lib/lenra/services/datastore_services.ex
@@ -2,16 +2,25 @@ defmodule Lenra.DatastoreServices do
   @moduledoc """
     The service that manages the different possible actions on a datastore.
   """
-  alias Lenra.{Datastore, Repo}
+  alias ApplicationRunner.DatastoreServices
+  alias Lenra.Repo
   require Logger
 
-  @doc """
-    Gets the datastore.
+  def create(environment_id, params) do
+    environment_id
+    |> DatastoreServices.create(params)
+    |> Repo.transaction()
+  end
 
-    Returns `nil` if the datastore does not exist.
-    Returns a `Lenra.Datastore` if the datastore exists.
-  """
-  def get_by(clauses) do
-    Repo.get_by(Datastore, clauses)
+  def update(datastore_id, params) do
+    datastore_id
+    |> DatastoreServices.update(params)
+    |> Repo.transaction()
+  end
+
+  def delete(datastore_id) do
+    datastore_id
+    |> DatastoreServices.delete()
+    |> Repo.transaction()
   end
 end
