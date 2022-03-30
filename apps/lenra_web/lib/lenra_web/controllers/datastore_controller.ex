ff22d81121d352b4320b3347be8ae69d1029cf1a
Emric Pichonnier <emric.pichonnier@lenra.me>
2022-03-30 09:22:40 +0200

    fix assign tuple

--- apps/lenra_web/lib/lenra_web/controllers/datastore_controller.ex
+++ apps/lenra_web/lib/lenra_web/controllers/datastore_controller.ex
@@ -6,7 +6,7 @@ defmodule LenraWeb.DatastoreController do
   def create(conn, params) do
     with {:ok, inserted_datastore: datastore} <- DatastoreServices.create(params["env_id"], params) do
       conn
-      |> assign_data(:datastore, datastore)
+      |> assign_data(:inserted_datastore, datastore)
       |> reply
     end
   end
@@ -14,7 +14,7 @@ defmodule LenraWeb.DatastoreController do
   def update(conn, params) do
     with {:ok, updated_datastore: datastore} <- DatastoreServices.update(params["datastore_id"], params) do
       conn
-      |> assign_data(:datastore, datastore)
+      |> assign_data(:updated_datastore, datastore)
       |> reply
     end
   end
@@ -22,7 +22,7 @@ defmodule LenraWeb.DatastoreController do
   def delete(conn, params) do
     with {:ok, deleted_datastore: datastore} <- DatastoreServices.delete(params["datastore_id"]) do
       conn
-      |> assign_data(:datastore, datastore)
+      |> assign_data(:deleted_datastore, datastore)
       |> reply
     end
   end
