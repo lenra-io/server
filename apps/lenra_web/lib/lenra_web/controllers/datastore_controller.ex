7c28a13c7ccec8d81bdf12960a9f7e7c884981f0
Emric Pichonnier <emric.pichonnier@gmail.com>
2022-03-31 16:01:17 +0200

    feat: add datastore controller (#178)

---
+++ apps/lenra_web/lib/lenra_web/controllers/datastore_controller.ex
@@ -0,0 +1,29 @@
+defmodule LenraWeb.DatastoreController do
+  use LenraWeb, :controller
+
+  alias Lenra.DatastoreServices
+
+  def create(conn, params) do
+    with {:ok, inserted_datastore: datastore} <- DatastoreServices.create(params["env_id"], params) do
+      conn
+      |> assign_data(:inserted_datastore, datastore)
+      |> reply
+    end
+  end
+
+  def update(conn, params) do
+    with {:ok, updated_datastore: datastore} <- DatastoreServices.update(params["datastore_id"], params) do
+      conn
+      |> assign_data(:updated_datastore, datastore)
+      |> reply
+    end
+  end
+
+  def delete(conn, params) do
+    with {:ok, deleted_datastore: datastore} <- DatastoreServices.delete(params["datastore_id"]) do
+      conn
+      |> assign_data(:deleted_datastore, datastore)
+      |> reply
+    end
+  end
+end
