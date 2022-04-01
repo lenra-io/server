6273cf76a02f75477d133110e071dc4a3bb88e46
Emric Pichonnier <emric.pichonnier@gmail.com>
2022-04-01 09:41:11 +0200

    feat: data_reference controller (#184)

---
+++ apps/lenra_web/test/controllers/data_reference_controller_test.exs
@@ -0,0 +1,40 @@
+defmodule LenraWeb.DataReferenceControllerTest do
+  @moduledoc """
+    Test the `LenraWeb.DataReferenceController` module
+  """
+
+  use LenraWeb.ConnCase, async: true
+
+  alias Lenra.{Environment, LenraApplication, Repo}
+
+  setup %{conn: conn} do
+    {:ok, %{conn: conn}}
+  end
+
+  defp create_app_and_get_env(conn) do
+    post(conn, Routes.apps_path(conn, :create), %{
+      "name" => "test",
+      "color" => "ffffff",
+      "icon" => 12
+    })
+
+    Repo.get_by(Environment, application_id: Enum.at(Repo.all(LenraApplication), 0).id)
+  end
+
+  # TODO make tests when route are defined
+  describe "LenraWeb.DataReferenceController.create_2/1" do
+    test "should create data if params valid" do
+    end
+
+    test "should return error if params not valid" do
+    end
+  end
+
+  describe "LenraWeb.DataReferenceController.delete_1/1" do
+    test "should delete data if id valid" do
+    end
+
+    test "should return error if id invalid" do
+    end
+  end
+end
