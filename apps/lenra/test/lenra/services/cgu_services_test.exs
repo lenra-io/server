a90eef897640fcc5e85da7847b3541f74c3f3f37
jonas-martinez <cbobbxta.fr@gmail.com>
2022-04-04 17:32:06 +0200

    fix formatting

--- apps/lenra/test/lenra/services/cgu_services_test.exs
+++ apps/lenra/test/lenra/services/cgu_services_test.exs
@@ -85,7 +85,9 @@ defmodule Lenra.CguServicesTest do
 
     test "not existing user", %{user: _user} do
       {:ok, cgu} = Repo.insert(Cgu.new(%{link: "test.html", hash: "a", version: "1.0.0"}))
-      assert {:error, :accepted_cgu, %{errors: [user_id: {"does not exist", _constraints}]}, _any} = CguServices.accept(cgu.id, -1)
+
+      assert {:error, :accepted_cgu, %{errors: [user_id: {"does not exist", _constraints}]}, _any} =
+               CguServices.accept(cgu.id, -1)
     end
 
     test "latest cgu", %{user: user} do
