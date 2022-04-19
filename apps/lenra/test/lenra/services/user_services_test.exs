53884ac9a288642ae02a63885ee4bb598b02038f
jonas-martinez <cbobbxta.fr@gmail.com>
2022-04-19 11:02:44 +0200

    Fix credo

--- apps/lenra/test/lenra/services/user_services_test.exs
+++ apps/lenra/test/lenra/services/user_services_test.exs
@@ -22,7 +22,7 @@ defmodule UserServicesTest do
     assert String.length(registration_code.code) == 8
   end
 
-  # TODO: Uncomment when the email service is enabled
+  # Uncomment when the email service is enabled
   # test "send email after registration" do
   #   {:ok, %{inserted_user: user, inserted_registration_code: registration_code}} = register_john_doe()
 
