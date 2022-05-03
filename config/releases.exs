8727c5971121f5ec05adbd0da9e112b04f673c3d
Emric Pichonnier <emric.pichonnier@lenra.io>
2022-05-03 11:29:14 +0200

    add env var

--- config/releases.exs
+++ config/releases.exs
@@ -25,7 +25,7 @@ config :lenra,
   gitlab_api_token: System.fetch_env!("GITLAB_API_TOKEN"),
   gitlab_project_id: System.fetch_env!("GITLAB_PROJECT_ID"),
   gitlab_ci_ref: "master",
-  template_url: "https://github.com/lenra-io/templates.git#data"
+  template_url: System.fetch_env!("TEMPLATE_URL")
 
 # Do not print debug messages in production
 config :logger, level: String.to_atom(System.get_env("LOG_LEVEL", "info"))
