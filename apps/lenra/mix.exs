ddbe831d0f26f4489d4e02ab150b93f33a431cad
Emric Pichonnier <emric.pichonnier@lenra.io>
2022-05-06 15:06:03 +0200

    feat: update application_runner

--- apps/lenra/mix.exs
+++ apps/lenra/mix.exs
@@ -52,7 +52,7 @@ defmodule Lenra.MixProject do
         name: :application_runner,
         host: "github.com",
         project: "lenra-io/application-runner.git",
-        tag: "v1.0.0-beta.30",
+        tag: "v1.0.0-beta.31",
         credentials: "shiipou:#{System.get_env("GH_PERSONNAL_TOKEN")}"
       )
     ]
