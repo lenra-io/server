850bea2c0d3a415ebbc3a75c75bc357800e06a31
Emric Pichonnier <emric.pichonnier@lenra.io>
2022-05-06 10:31:11 +0200

    feat: add rourte to query user_data

--- apps/lenra_web/lib/lenra_web/controllers/data_controller.ex
+++ apps/lenra_web/lib/lenra_web/controllers/data_controller.ex
@@ -22,6 +22,15 @@ defmodule LenraWeb.DataController do
     end
   end
 
+  def get_me(conn, params) do
+    with session_assings <- Plug.current_resource(conn),
+         result <- DataServices.get_me(session_assings.environment.id, session_assings.user.id) do
+      conn
+      |> assign_data(:user_data, result)
+      |> reply
+    end
+  end
+
   def create(conn, params) do
     with session_assings <- Plug.current_resource(conn),
          {:ok, %{inserted_data: data}} <-
