637197da9d9c590ef15420027a1a3dcedecfc2c7
Emric Pichonnier <emric.pichonnier@lenra.io>
2022-07-19 10:00:35 +0200

    fix: fix rebase error

--- apps/lenra/test/lenra/apps/app_test.exs
+++ apps/lenra/test/lenra/apps/app_test.exs
@@ -7,7 +7,7 @@ defmodule Lenra.Apps.AppTest do
   @valide_data %{name: "Test", color: "FF0000", icon: 1111, repository: "repository"}
   @invalide_data %{name: nil, color: nil, icon: nil, repository: nil}
 
-  describe "lenra_appliaction" do
+  describe "lenra_application" do
     test "new/2 with valid data creates a lenra_application" do
       assert %{changes: app, valid?: true} = App.new(1, @valide_data)
       assert app.name == @valide_data.name
@@ -18,35 +18,6 @@ defmodule Lenra.Apps.AppTest do
 
     test "new/2 with invalid data creates a lenra_application" do
       assert %{changes: _app, valid?: false} = App.new(1, @invalide_data)
-    end
-
-    test "attached to app_user_session should succed" do
-      # Create user
-      {:ok, %{inserted_user: user}} = UserTestHelper.register_john_doe()
-
-      # Create and insert lenra_application
-      app = App.new(user.id, @valide_data)
-      {:ok, %App{} = inserted_app} = Repo.insert(app)
-
-      # Test create and insert app_user_session
-      session_uuid = Ecto.UUID.generate()
-
-      session =
-        AppUserSession.new(%{
-          uuid: session_uuid,
-          user_id: user.id,
-          application_id: inserted_app.id,
-          build_number: 1
-        })
-
-      assert %{valid?: true} = session
-
-      {:ok, %AppUserSession{} = inserted_session} = Repo.insert(session)
-      loaded_inserted_session = Repo.preload(inserted_session, :application)
-      assert loaded_inserted_session.application == inserted_app
-
-      [head | _tail] = Repo.all(Ecto.assoc(inserted_app, :app_user_session))
-      assert head == inserted_session
     end
   end
 end
