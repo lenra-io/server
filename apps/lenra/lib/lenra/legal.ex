2201150e32e8f37dedd48941b3dc3cb271974a08
ClementGld <clement.guillaud@lenra.io>
2022-07-25 14:51:24 +0200

    fix credo

--- apps/lenra/lib/lenra/legal.ex
+++ apps/lenra/lib/lenra/legal.ex
@@ -8,7 +8,7 @@ defmodule Lenra.Legal do
 
   import Ecto.Query, only: [from: 2, select: 3]
 
-  alias Lenra.Errors.BusinessError
+  alias Lenra.Errors.{BusinessError, TechnicalError}
   alias Lenra.Legal
   alias Lenra.Legal.{CGU, UserAcceptCGUVersion}
   alias Lenra.Repo
@@ -18,7 +18,7 @@ defmodule Lenra.Legal do
     cgu = get_latest_cgu_query() |> Repo.one()
 
     case cgu do
-      nil -> Lenra.Errors.TechnicalError.cgu_not_found_tuple()
+      nil -> TechnicalError.cgu_not_found_tuple()
       cgu -> {:ok, cgu}
     end
   end
