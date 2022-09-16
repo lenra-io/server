5d946a4945e97e77541259a27659fff001fdf0c5
Emric Pichonnier <emric.pichonnier@lenra.me>
2022-09-16 10:55:16 +0200

    feat: add moduledoc

--- apps/lenra_web/lib/lenra_web/channels/app_channel.ex
+++ apps/lenra_web/lib/lenra_web/channels/app_channel.ex
@@ -1,3 +1,6 @@
 defmodule LenraWeb.AppChannel do
+  @moduledoc """
+    LenraWeb.AppChannel use ApplicationRunner.AppChannel and pass the adapter module
+  """
   use ApplicationRunner.AppChannel, adapter: LenraWeb.AppAdapter
 end
