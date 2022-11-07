8da39651748962dc1193bd6e1bba265d4e5b5064
jonas-martinez <cbobbxta.fr@gmail.com>
2022-11-07 10:53:00 +0100

    cleaning

--- apps/lenra/lib/lenra/application.ex
+++ apps/lenra/lib/lenra/application.ex
@@ -44,7 +44,7 @@ defmodule Lenra.Application do
 
     Logger.info("Lenra Supervisor Starting")
     res = Supervisor.start_link(children, opts)
-    Application.ensure_all_started(:application_runner) |> IO.inspect
+    Application.ensure_all_started(:application_runner)
 
     Lenra.Seeds.run()
     Logger.info("Lenra Supervisor Started")
