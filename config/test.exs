a13d941dd32a1684095e012f3bfac6b00be0c0b6
jonas-martinez <cbobbxta.fr@gmail.com>
2022-11-18 10:01:31 +0100

    fix test

--- config/test.exs
+++ config/test.exs
@@ -32,4 +32,12 @@ config :lenra, Lenra.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   queue_target: 500
 
+config :application_runner, ApplicationRunner.Repo,
+  username: "postgres",
+  password: "postgres",
+  database: "lenra_test#{System.get_env("MIX_TEST_PARTITION")}",
+  hostname: System.get_env("POSTGRES_HOST", "localhost"),
+  pool: Ecto.Adapters.SQL.Sandbox,
+  queue_target: 500
+
 config :lenra, Lenra.Mailer, adapter: Bamboo.TestAdapter
