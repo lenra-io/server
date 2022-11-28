1a7ed3f50d4310cd53b436293485e5bf85343d6b
jonas-martinez <cbobbxta.fr@gmail.com>
2022-11-28 14:39:08 +0100

    update dependencies

--- apps/lenra/mix.exs
+++ apps/lenra/mix.exs
@@ -50,7 +50,7 @@ defmodule Lenra.MixProject do
       {:libcluster, "~> 3.3"},
       {:application_runner,
         git: "https://github.com/lenra-io/application-runner.git",
-        tag: "b904bee",
+        tag: "4bc2e2c",
         submodules: true
       },
       {:lenra_common, git: "https://github.com/lenra-io/lenra-common.git", tag: "v2.4.0"}
