e75f4823561d4389690551d17c0d8e490f953801
jonas-martinez <cbobbxta.fr@gmail.com>
2022-12-20 16:54:51 +0100

    oops, this was useful for CGUs

--- mix.exs
+++ mix.exs
@@ -61,7 +61,8 @@ defmodule Lenra.Umbrella.MixProject do
       setup: ["deps.get", "ecto.setup"],
       "ecto.setup": [
         "ecto.create",
-        "ecto.migrate --migrations-path apps/lenra/priv/repo/migrations --migrations-path deps/application_runner/priv/repo/migrations"
+        "ecto.migrate --migrations-path apps/lenra/priv/repo/migrations --migrations-path deps/application_runner/priv/repo/migrations",
+        "run apps/lenra/priv/repo/seeds.exs"
       ],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: [
