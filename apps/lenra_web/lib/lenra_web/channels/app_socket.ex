eb50a7a61631b7f7a85ef63973a2cdee9afafda5
jonas-martinez <cbobbxta.fr@gmail.com>
2023-04-05 15:20:06 +0200

    Forgot this

--- apps/lenra_web/lib/lenra_web/channels/app_socket.ex
+++ apps/lenra_web/lib/lenra_web/channels/app_socket.ex
@@ -1,5 +1,6 @@
 defmodule LenraWeb.AppSocket do
   use ApplicationRunner.AppSocket,
     adapter: LenraWeb.AppAdapter,
-    route_channel: LenraWeb.RouteChannel
+    route_channel: LenraWeb.RouteChannel,
+    routes_channel: DevTool.RoutesChannel
 end
