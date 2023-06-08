b2a7e841f5419a07d0dd9ee3100de71a938e5356
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2023-06-08 10:47:03 +0200

    feat: Lenra routes (#385)
    * feat: Lenra Routes (#367)
    
    * feat: Lenra Routes
    
    * Forgot this
    
    * Update deps
    
    * fix
    
    * feat: Update dependencies
    
    * feat: Update dependencies
    
    * feat: Update dependencies (#391)
    
    * fix: Revert libcluster Kubernetes
    
    * feat: Revert to Kubernetes libcluster strategy
    
    * revert: previous commit
    
    * build(deps): application_runner beta version
    
    * fix: runtime config file
    
    ---------
    
    Co-authored-by: Thomas DA ROCHA <thomas.darocha@lenra.io>

---
+++ apps/lenra_web/lib/lenra_web/channels/routes_channel.ex
@@ -0,0 +1,6 @@
+defmodule LenraWeb.RoutesChannel do
+  @moduledoc """
+    `LenraWeb.AppChannel` handle the app channel to run app and listeners and push to the user the resulted UI or Patch
+  """
+  use ApplicationRunner.RoutesChannel
+end
--- config/runtime.exs
+++ config/runtime.exs
@@ -3,86 +3,86 @@
 
 import Config
 
-config :lenra_web, LenraWeb.Endpoint,
-  http: [port: System.fetch_env!("PORT")],
-  secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
-  url: [host: System.fetch_env!("API_ENDPOINT"), port: System.fetch_env!("PORT")]
+if config_env() == :prod do
+  config :lenra_web, LenraWeb.Endpoint,
+    http: [port: System.fetch_env!("PORT")],
+    secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
+    url: [host: System.fetch_env!("API_ENDPOINT"), port: System.fetch_env!("PORT")]
 
-config :lenra, Lenra.Repo,
-  username: System.fetch_env!("POSTGRES_USER"),
-  password: System.fetch_env!("POSTGRES_PASSWORD"),
-  database: System.fetch_env!("POSTGRES_DB"),
-  hostname: System.fetch_env!("POSTGRES_HOST")
+  config :lenra, Lenra.Repo,
+    username: System.fetch_env!("POSTGRES_USER"),
+    password: System.fetch_env!("POSTGRES_PASSWORD"),
+    database: System.fetch_env!("POSTGRES_DB"),
+    hostname: System.fetch_env!("POSTGRES_HOST")
 
-config :lenra,
-  faas_url: System.fetch_env!("FAAS_URL"),
-  faas_auth: System.fetch_env!("FAAS_AUTH"),
-  faas_registry: System.fetch_env!("FAAS_REGISTRY"),
-  runner_secret: System.fetch_env!("RUNNER_SECRET"),
-  runner_callback_url: "https://#{System.fetch_env!("APP_HOST")}",
-  lenra_env: System.fetch_env!("ENVIRONMENT"),
-  gitlab_api_url: System.fetch_env!("GITLAB_API_URL"),
-  gitlab_api_token: System.fetch_env!("GITLAB_API_TOKEN"),
-  gitlab_project_id: System.fetch_env!("GITLAB_PROJECT_ID"),
-  gitlab_ci_ref: "master",
-  template_url: System.fetch_env!("TEMPLATE_URL"),
-  lenra_email: System.fetch_env!("LENRA_EMAIL"),
-  lenra_app_url: System.fetch_env!("LENRA_APP_URL")
+  config :lenra,
+    faas_url: System.fetch_env!("FAAS_URL"),
+    faas_auth: System.fetch_env!("FAAS_AUTH"),
+    faas_registry: System.fetch_env!("FAAS_REGISTRY"),
+    runner_secret: System.fetch_env!("RUNNER_SECRET"),
+    runner_callback_url: "https://#{System.fetch_env!("APP_HOST")}",
+    lenra_env: System.fetch_env!("ENVIRONMENT"),
+    gitlab_api_url: System.fetch_env!("GITLAB_API_URL"),
+    gitlab_api_token: System.fetch_env!("GITLAB_API_TOKEN"),
+    gitlab_project_id: System.fetch_env!("GITLAB_PROJECT_ID"),
+    gitlab_ci_ref: "master",
+    template_url: System.fetch_env!("TEMPLATE_URL"),
+    lenra_email: System.fetch_env!("LENRA_EMAIL"),
+    lenra_app_url: System.fetch_env!("LENRA_APP_URL")
 
-config :application_runner,
-  url: System.fetch_env!("API_ENDPOINT"),
-  faas_url: System.fetch_env!("FAAS_URL"),
-  faas_auth: System.fetch_env!("FAAS_AUTH"),
-  faas_registry: System.fetch_env!("FAAS_REGISTRY"),
-  gitlab_api_url: System.fetch_env!("GITLAB_API_URL"),
-  listeners_timeout: String.to_integer(System.fetch_env!("LISTENERS_TIMEOUT")),
-  view_timeout: String.to_integer(System.fetch_env!("VIEW_TIMEOUT")),
-  manifest_timeout: String.to_integer(System.fetch_env!("MANIFEST_TIMEOUT")),
-  env: System.fetch_env!("ENVIRONMENT")
+  config :application_runner,
+    url: System.fetch_env!("API_ENDPOINT"),
+    faas_url: System.fetch_env!("FAAS_URL"),
+    faas_auth: System.fetch_env!("FAAS_AUTH"),
+    faas_registry: System.fetch_env!("FAAS_REGISTRY"),
+    gitlab_api_url: System.fetch_env!("GITLAB_API_URL"),
+    listeners_timeout: String.to_integer(System.fetch_env!("LISTENERS_TIMEOUT")),
+    view_timeout: String.to_integer(System.fetch_env!("VIEW_TIMEOUT")),
+    manifest_timeout: String.to_integer(System.fetch_env!("MANIFEST_TIMEOUT")),
+    env: System.fetch_env!("ENVIRONMENT")
 
-config :application_runner, ApplicationRunner.Repo,
-  username: System.fetch_env!("POSTGRES_USER"),
-  password: System.fetch_env!("POSTGRES_PASSWORD"),
-  database: System.fetch_env!("POSTGRES_DB"),
-  hostname: System.fetch_env!("POSTGRES_HOST"),
-  show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  config :application_runner, ApplicationRunner.Repo,
+    username: System.fetch_env!("POSTGRES_USER"),
+    password: System.fetch_env!("POSTGRES_PASSWORD"),
+    database: System.fetch_env!("POSTGRES_DB"),
+    hostname: System.fetch_env!("POSTGRES_HOST"),
+    show_sensitive_data_on_connection_error: true,
+    pool_size: 10
 
-config :application_runner, :mongo,
-  hostname: System.fetch_env!("MONGO_HOSTNAME"),
-  port: System.get_env("MONGO_PORT", "27017"),
-  username: System.get_env("MONGO_USERNAME"),
-  password: System.get_env("MONGO_PASSWORD"),
-  ssl: System.get_env("MONGO_SSL", "false"),
-  auth_source: System.get_env("MONGO_AUTH_SOURCE")
+  config :application_runner, :mongo,
+    hostname: System.fetch_env!("MONGO_HOSTNAME"),
+    port: System.get_env("MONGO_PORT", "27017"),
+    username: System.get_env("MONGO_USERNAME"),
+    password: System.get_env("MONGO_PASSWORD"),
+    ssl: System.get_env("MONGO_SSL", "false"),
+    auth_source: System.get_env("MONGO_AUTH_SOURCE")
 
-# Do not print debug messages in production
-config :logger, level: String.to_atom(System.get_env("LOG_LEVEL", "info"))
+  # Do not print debug messages in production
+  config :logger, level: String.to_atom(System.get_env("LOG_LEVEL", "info"))
 
-config :libcluster,
-  topologies: [
-    lenra: [
-      # The selected clustering strategy. Required.
-      strategy: Elixir.Cluster.Strategy.Kubernetes,
-      # Configuration for the provided strategy. Optional.
-      config: [
-        mode: :ip,
-        kubernetes_selector: System.fetch_env!("KUBERNETES_SELECTOR"),
-        kubernetes_namespace: System.fetch_env!("KUBERNETES_NAMESPACE"),
-        application_name: "lenra",
-        polling_interval: 10_000
+  config :libcluster,
+    topologies: [
+      lenra: [
+        # The selected clustering strategy. Required.
+        strategy: Elixir.Cluster.Strategy.Kubernetes.DNS,
+        # Configuration for the provided strategy. Optional.
+        config: [
+          service: System.fetch_env!("SERVICE_NAME"),
+          application_name: "lenra",
+          polling_interval: 10_000
+        ]
       ]
     ]
-  ]
 
-config :lenra, Lenra.Mailer, api_key: System.fetch_env!("SENDGRID_API_KEY")
+  config :lenra, Lenra.Mailer, api_key: System.fetch_env!("SENDGRID_API_KEY")
 
-config :lenra_web, LenraWeb.Guardian, secret_key: System.fetch_env!("GUARDIAN_SECRET_KEY")
+  config :lenra_web, LenraWeb.Guardian, secret_key: System.fetch_env!("GUARDIAN_SECRET_KEY")
 
-config :lenra_web, LenraWeb.AppGuardian, secret_key: System.fetch_env!("GUARDIAN_SECRET_KEY")
+  config :lenra_web, LenraWeb.AppGuardian, secret_key: System.fetch_env!("GUARDIAN_SECRET_KEY")
 
-# Set the DSN for sentry and the current environment.
-# Only production and sentry are used. test env are ignored.
-config :sentry,
-  dsn: System.fetch_env!("SENTRY_DSN"),
-  environment_name: System.fetch_env!("ENVIRONMENT")
+  # Set the DSN for sentry and the current environment.
+  # Only production and sentry are used. test env are ignored.
+  config :sentry,
+    dsn: System.fetch_env!("SENTRY_DSN"),
+    environment_name: System.fetch_env!("ENVIRONMENT")
+end
