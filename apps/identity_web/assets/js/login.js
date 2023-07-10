e230ef992a70bfdfac5c5dd06d57a78dae7433cc
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-07-10 12:52:18 +0200

    feat: Disable form while invalid

--- apps/identity_web/assets/js/login.js
+++ apps/identity_web/assets/js/login.js
@@ -2,12 +2,21 @@
     const form = document.querySelector("form");
     const submitButton = form.querySelector('button[type="submit"]');
     form.onsubmit = function (event) {
-        const currentAction = form.querySelector('input[name="user[submit_action]"]:checked');
-        form.action = currentAction.dataset.formAction;
+        setSubmitAction();
     };
     form.onchange = function (event) {
+        setSubmitAction();
+        checkValidity();
+    };
+    form.oninput = function (event) {
+        checkValidity();
+    };
+    function setSubmitAction() {
         const currentAction = form.querySelector('input[name="user[submit_action]"]:checked');
         submitButton.innerText = currentAction.dataset.submit;
         form.action = currentAction.dataset.formAction;
-    };
+    }
+    function checkValidity() {
+        submitButton.disabled = !form.checkValidity();
+    }
 })();
