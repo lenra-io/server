db2fda9b7b83439efe2501db7602e86191ab0e2f
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-07-12 09:56:09 +0200

    feat: Add show password buttton

--- apps/identity_web/assets/js/login.js
+++ apps/identity_web/assets/js/login.js
@@ -1,5 +1,6 @@
 (() => {
     const form = document.querySelector("form");
+    const passwordFieldset = form.querySelector('fieldset.password');
     const submitButton = form.querySelector('button[type="submit"]');
     form.onsubmit = function (event) {
         setSubmitAction();
@@ -18,5 +19,17 @@
     }
     function checkValidity() {
         submitButton.disabled = !form.checkValidity();
+    }
+    if (passwordFieldset) {
+        const passwordLabel = passwordFieldset.querySelector('label');
+        const passwordInput = passwordFieldset.querySelector('input[type="password"]');
+        passwordLabel.onclick = function (event) {
+            if (event.layerY > event.target.offsetHeight) {
+                const shown = passwordInput.type === 'text';
+                passwordInput.type = shown ? 'password' : 'text';
+                passwordLabel.classList.toggle('lenra-icon-eye');
+                passwordLabel.classList.toggle('lenra-icon-eye-off');
+            }
+        }
     }
 })();
