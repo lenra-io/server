95aa5249900bca52ef190004ffee2b80b80b00c6
shiipou <shiishii@nocturlab.fr>
2023-08-30 16:18:38 +0200

    fix: correct way to use genserver.

--- apps/lenra/lib/lenra/services/kubernetes_check_status_service.ex
+++ apps/lenra/lib/lenra/services/kubernetes_check_status_service.ex
@@ -1,108 +1,85 @@
-defmodule Lenra.Services.KubernetesCheckStatusService do
+defmodule Lenra.KubernetesCheckStatusService do
   use GenServer
 
-  def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, [], opts)
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
-  def init([]) do
-    {:ok, %{pods_to_check: [], timer_ref: nil}}
+  def add_job(job_name, namespace, build_id) do
+    GenServer.cast(__MODULE__, {:add_job, %{job_name: job_name, namespace: namespace, build_id: build_id}})
   end
 
-  def handle_call({:get_pods_to_check}, _from, state) do
-    {:reply, state[:pods_to_check], state}
+  def init(_) do
+    {:ok, %{jobs: [], current_index: 0}}
   end
 
-  def handle_call({:get_timer_ref}, _from, state) do
-    {:reply, state[:timer_ref], state}
+  def handle_cast({:add_job, job}, %{jobs: []} = state) do
+    Process.send_after(self(), :process_jobs, 5000)
+    {:noreply, %{state | jobs: [job]}}
   end
 
-  def handle_call({:clear_pods_to_check}, _from, state) do
-    {:reply, :ok, %{state | pods_to_check: []}}
+  def handle_cast({:add_job, job}, state) do
+    {:noreply, %{state | jobs: state.jobs ++ [job]}}
   end
 
-  def handle_call({:add_pod_to_check, pod_name, namespace, build_id}, _from, state) do
-    new_pods_to_check = state[:pods_to_check] ++ {:running, pod_name, namespace, build_id}
-    if new_pods_to_check == [] do
-      {:reply, :ok, %{state | pods_to_check: new_pods_to_check, timer_ref: 5000}}
-    else
-      {:reply, :ok, %{state | pods_to_check: new_pods_to_check}}
+  def handle_info(:process_jobs, state) do
+    state = process_job(state)
+    {:noreply, state}
+  end
+
+  defp process_job(%{jobs: [], current_index: _}) do
+    %{jobs: [], current_index: 0}
+  end
+
+  defp process_job(%{jobs: jobs, current_index: index} = state) when index < length(jobs) do
+    job = Enum.at(jobs, index)
+    status = check_job_status(job)
+
+    case status do
+      :succeeded ->
+        # TODO: Set build status to succeed using build_id
+        # Remove the job from the queue
+        %{state | jobs: List.delete_at(jobs, index)}
+
+      :failed ->
+        # TODO: Set build status to failure using build_id and reasons
+        # Remove the job from the queue
+        %{state | jobs: List.delete_at(jobs, index)}
+
+      _ ->
+        # Move to the next job
+        %{state | current_index: index + 1}
     end
   end
 
-  def handle_info(:check_pods_status, state) do
-    case state[:pods_to_check] do
-      [] ->
-        {:noreply, %{state | timer_ref: nil}}
-      pods ->
-        case state[:cur_index] do
-          length(pods) ->
-            Process.send_after(self(), :check_pods_status, 5000)
-            {:noreply, %{state | pods_to_check: pods, cur_index: 0}}
-          _ ->
-        {pod, remaining_pods} = List.pop_at(pods, state[:cur_index])
-        case get_pod_status(pod[:pod_name], pod[:namespace], pod[:build_id]) do
-          {:ok, "success"} ->
-            update_build_status(pod[:build_id], "succeeded")
-            {:noreply, %{state | pods_to_check: remaining_pods}}
-          {:ok, "failure"} ->
-            update_build_status(pod[:build_id], "failed")
-            {:noreply, %{state | pods_to_check: remaining_pods}}
-          {:ok, _} ->
-            {:noreply, %{state | pods_to_check: pods, cur_index: state[:cur_index] + 1}}
-          {:error, _reason} ->
-            update_build_status(pod[:build_id], "failed")
-            {:noreply, %{state | pods_to_check: remaining_pods}}
-        end
-      end
-    end
+  defp process_job(state) do
+    Process.send_after(self(), :process_jobs, 5000)
+    %{state | current_index: 0}
   end
 
-  defp get_pod_status(pod_name, namespace, build_id) do
+  defp check_job_status(job) do
     kubernetes_api_url = Application.fetch_env!(:lenra, :kubernetes_api_url)
     kubernetes_api_token = Application.fetch_env!(:lenra, :kubernetes_api_token)
 
-    url = "#{kubernetes_api_url}/api/v1/namespaces/#{namespace}/pods/#{pod_name}/status"
+    url = "#{kubernetes_api_url}/api/v1/namespaces/#{job[:namespace]}/pods/#{job[:job_name]}/status"
     headers = [{"Authorization", "Bearer #{kubernetes_api_token}"}]
 
-    Finch.build(:get, url, headers)
-    |> Finch.request(PipelineHttp)
-    |> Stream.map(&Jason.decode!(&1.body))
-    |> Stream.map(&extract_pod_status(&1))
-    |> Stream.map(&update_build_status(&1, build_id))
-    |> Stream.run()
-  end
+    response =
+      Finch.build(:get, url, headers)
+      |> Finch.request(PipelineHttp)
+      |> Stream.map(&Jason.decode!(&1.body))
+      |> Stream.map(&extract_job_status(&1))
+      |> Stream.run()
 
-  defp extract_pod_status(%{"phase" => phase, "containerStatuses" => container_statuses, "initContainerStatuses" => init_container_statuses}) do
-    %{
-      phase: phase,
-      container_statuses: container_statuses |> Enum.map(&extract_container_status/1),
-      init_container_statuses: init_container_statuses |> Enum.map(&extract_container_status/1)
-    }
-  end
-
-  defp extract_container_status(%{"name" => name, "state" => state}) do
-    case state do
-      %{"running" => info} ->
-        %{
-          name: name,
-          state: "running"
-        }
-      %{"terminated" => %{"reason" => reason}} ->
-        %{
-          name: name,
-          state: reason
-        }
-      %{"waiting" => %{"reason" => reason}} ->
-        %{
-          name: name,
-          state: reason
-        }
+    case response do
+      %{"status" => %{"succeeded" => 1}} -> :succeeded
+      %{"status" => %{"failed" => 1}} -> :failed
+      _ -> :unknown
     end
   end
 
-  defp update_build_status(build_id, status) do
-    # Update build status to "succeeded" or "failed"
-    # ...
+  defp extract_job_status(response) do
+    # Extract the job status from the response
+    response["status"]
   end
 end
