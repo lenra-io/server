7e1acade4be8ecd0a38dc7b52cda3d30e7c8ba82
jonas.martinez <cbobbxta.fr@gmail.com>
2023-12-14 11:36:42 +0100

    Undo unrelated change

--- libs/application_runner/test/controllers/docs_controller_test.exs
+++ libs/application_runner/test/controllers/docs_controller_test.exs
@@ -92,7 +92,7 @@ defmodule ApplicationRunner.DocsControllerTest do
       conn =
         conn
         |> Plug.Conn.put_req_header("authorization", "Bearer " <> token)
-        |> post(Routes.docs_path(conn, :find, @coll), %{"query" => %{"_id" => doc_id}})
+        |> post(Routes.docs_path(conn, :find, @coll), %{"_id" => doc_id})
 
       assert [%{"foo" => "bar"}] = json_response(conn, 200)
     end
