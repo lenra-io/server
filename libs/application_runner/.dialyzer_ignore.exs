26d1f93e3febf278cd41782896ea5dc739619b9b
jonas.martinez <cbobbxta.fr@gmail.com>
2024-02-05 22:50:35 +0100

    fix dialyzer

--- libs/application_runner/.dialyzer_ignore.exs
+++ libs/application_runner/.dialyzer_ignore.exs
@@ -1,4 +1,4 @@
 [
-  {"lib/mongo_storage.ex", :pattern_match, 112},
-  {"lib/mongo_storage.ex", :pattern_match, 93}
+  {"lib/mongo_storage.ex", :pattern_match, 108},
+  {"lib/mongo_storage.ex", :pattern_match, 89}
 ]
